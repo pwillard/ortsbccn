6692c58b54f685a39eec912648a648fd07a6102e
Pete Willard <petewillard@gmail.com>
2023-01-31 13:11:06 -0500

    added Marek's AO Notes

--- do.bat
+++
@@ -1,1 +0,0 @@
-asciidoctor-pdf -a pdf-themesdir=themes -a pdf-theme=test -a pdf-fontsdir=fonts book.adoc -o notebook.pdf
---
+++ makeit.bat
@@ -0,0 +1,8 @@
+@echo Started: %date% %time%
+call asciidoctor-pdf -a pdf-style=resources/pdfstyles/screen-theme.yml -a pdf-fontsdir=fonts book.adoc -o ORTS-CC-Notebook.pdf
+@echo Ended: %date% %time% 
+rem asciidoctor-pdf  book.adoc
+if exist *.png (
+    rm *.png
+)
+
